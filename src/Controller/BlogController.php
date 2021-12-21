52e76734bb39f4573f88fded1e1466d5211de583
lagupa <lagustephen@gmail.com>
2021-12-20 23:08:26 -0600

    adding forms"

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,35 +1,44 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Blog;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class BlogController extends AbstractController
 {
+    // LIST BLOG
+
     /**
-    * @Route("/blogs/")
+    * @Route("/blogs/", name="blog_list")
     * @Method({"GET"})
     */
-    public function readBlogs(): Response
+    public function readBlogs(ManagerRegistry $doctrine): Response
     {
-         $blogs = [
-            (object) [
-                "id" => 1,
-                'title' => 'Blog 1',
-                'content' => 'This is blog content 1'
-            ],
-            (object) [
-                "id" => 2,
-                'title' => 'Blog 2',
-                'content' => 'This is blog content 2'
-            ],
-        ];
-
+        $blogs = $doctrine->getRepository(Blog::class)->findAll();
         return $this->render(
             'blogs/read-blog.html.twig',
             ["blogs"=>$blogs]
+        );
+    }
+
+    // BLOG DETAILS
+
+    /**
+    * @Route("/blogs/{id}", name="blog_detail")
+    * @Method({"GET"})
+    */
+    public function readBlogsDetails(ManagerRegistry $doctrine, int $id): Response
+    {
+        $blog = $doctrine->getRepository(Blog::class)->find($id);
+        return $this->render(
+            'blogs/read-blog-detail.html.twig',
+            ["blog"=>$blog]
         );
     }
 }

43bbd3e2edfc50d98ff1144201341b5022e8217f
lagupa <lagustephen@gmail.com>
2021-12-21 11:37:20 -0600

    delete, detail blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,10 +4,15 @@ namespace App\Controller;
 use App\Entity\Blog;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 
 class BlogController extends AbstractController
@@ -15,7 +20,7 @@ class BlogController extends AbstractController
     // LIST BLOG
 
     /**
-    * @Route("/blogs/", name="blog_list")
+    * @Route("/blogs/", name="list_blog")
     * @Method({"GET"})
     */
     public function readBlogs(ManagerRegistry $doctrine): Response
@@ -27,13 +32,51 @@ class BlogController extends AbstractController
         );
     }
 
+
+    /**
+    * @Route("/blogs/create", name="create_blog")
+    * @Method({"GET", "POST"})
+    */
+    public function createBlog(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $blog = new Blog();
+
+        $form = $this->createFormBuilder($blog)
+        ->add('title', TextType::class,
+           array('attr' => array('class' => "form-control")))
+        ->add('content', TextareaType::class, array(
+            'required' => False, 'attr' => array('class' => 'btn btn-primary')))
+        ->add('save', SubmitType::class, array('label' => 'Create','attr' => array('class' => 'btn btn-primary')))
+        ->getForm();
+
+        // Process Form Data
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $newBlog = $form->getData();
+
+            // save form data into database;
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($newBlog);
+            $entityManager->flush();
+            return $this->redirectToRoute('list_blog');
+        }
+
+
+        return $this->render(
+            'blogs/create-blog.html.twig',
+            ["form"=>$form->createView()]
+        );
+    }
+
+
     // BLOG DETAILS
 
     /**
-    * @Route("/blogs/{id}", name="blog_detail")
+    * @Route("/blogs/detail/{id}", name="detail_blog")
     * @Method({"GET"})
     */
-    public function readBlogsDetails(ManagerRegistry $doctrine, int $id): Response
+    public function readBlogsDetails(ManagerRegistry $doctrine, $id): Response
     {
         $blog = $doctrine->getRepository(Blog::class)->find($id);
         return $this->render(
@@ -41,4 +84,28 @@ class BlogController extends AbstractController
             ["blog"=>$blog]
         );
     }
+    // DELETE BLOG
+
+    /**
+    * @Route("/blogs/delete/{id}", name="delete_blog")
+    * @Method({"DELETE"})
+    */
+    public function deleteBlog(ManagerRegistry $doctrine, $id): Response
+    {
+        // find blog to be deleted
+        $blog = $doctrine->getRepository(Blog::class)->find($id);
+
+        // create entity manager
+        $entityManager = $doctrine->getManager();
+
+        // delete blog
+        $entityManager->remove($blog);
+
+        // refresh db
+        $entityManager->flush();
+
+        return $this->redirectToRoute('list_blog');
+    }
+
+
 }
